8146d958790adc316d6060daa81b7c1af0ce8d46
starlash7 <yang92sw@gmail.com>
2024-12-23 23:40:22 +0900

    2.3 구조분해할당

---
+++ section02/chapter03.js
@@ -0,0 +1,28 @@
+// 1. 배열의 구조 분해 할당
+let arr = [1, 2, 3];
+
+// let one = arr[0];
+// let two = arr[1];
+// let three = arr[3];
+
+let [one, two, three, four = 4] = arr;
+
+// 2. 객체의 구조 분해 할당
+let person = {
+  name: "이정환",
+  age: 27,
+  hobby: "테니스",
+};
+
+// let name = person.name;
+// let age = person.age;
+// let hobby = person.hobby;
+
+let { name, age, hobby } = person;
+
+// 3. 객체 구조 분해 할당을 이용해서 함수의 매개변수를 받는 방법
+const func = ({ name, age, hobby }) => {
+  console.log(name, age, hobby);
+};
+
+func(person);
