036ced1f4f53c0f80e6eb17eb4a7e16098896f66
starlash7 <yang92sw@gmail.com>
2024-12-25 21:21:59 +0900

    3.4 Nodejs 모듈

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,6 @@
-console.log("안녕 Node.js");
+// const { add, sub } = require("./math");
+import mul, { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
---
+++ section03/src/math.js
@@ -0,0 +1,18 @@
+// math 모듈
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+// module.exports = {
+//   add: add,
+//   sub: sub,
+// };
+
+export default function multiplay(a, b) {
+  return a * b;
+}
