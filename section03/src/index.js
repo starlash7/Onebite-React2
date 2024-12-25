05efab88405663d0da9e7e54a2a29f2f7720fa15
starlash7 <yang92sw@gmail.com>
2024-12-25 21:44:40 +0900

    3.5 Nodejs 라이브러리

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,6 +1,11 @@
 // const { add, sub } = require("./math");
 import mul, { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
