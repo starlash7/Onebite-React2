1c48c34cf95460e9f82cc4cef85a3140edf032c7
starlash7 <yang92sw@gmail.com>
2024-12-27 23:53:14 +0900

    5.4 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button text={"메일"} color={"red"} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <div>자식요소</div>
+      </Button>
     </>
   );
 }
