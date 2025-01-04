1bd7b470bc819ef431e7d0c1aa4f9fd0fa0f738a
starlash7 <yang92sw@gmail.com>
2025-01-05 02:18:34 +0900

    8.2 UI set

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import List from "./components/List";
+import Editor from "./components/Editor";
 
 function App() {
-  return <>Todolist</>;
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
