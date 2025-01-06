e4852e43061b3bcc097f7b18d2a57d443ca85da7
starlash7 <yang92sw@gmail.com>
2025-01-06 15:33:09 +0900

    8.3 기능구현 준비

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,31 @@ import "./App.css";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "밥먹기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "운동하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState();
   return (
     <div className="App">
       <Header />
