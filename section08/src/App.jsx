ff5386b671ba24efc95ae70a0059eb4cdb5162d1
starlash7 <yang92sw@gmail.com>
2025-01-06 15:47:58 +0900

    8.4 Create- Todo

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const mockData = [
   {
@@ -26,11 +26,24 @@ const mockData = [
 ];
 
 function App() {
-  const [todos, setTodos] = useState();
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
